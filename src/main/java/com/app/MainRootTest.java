3657b94a470b941bf77965a41301abca5fd8905c
andyluo882016 <andylu882016>
2022-02-13 22:48:06 -0800

    first commit v2 @2/16

--- src/main/java/com/app/MainRootTest.java
+++ src/main/java/com/app/MainRootTest.java
@@ -5,6 +5,6 @@ public class MainRootTest {
     public static void main(String[] args){
         System.out.println("this is root top paranet");
         System.out.println("Add one new line for testing at root class");
-        System.out.println("SubBranche make change");
+        System.out.println("SubBranche make chang");
     }
 }
