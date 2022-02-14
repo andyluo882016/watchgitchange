b6ba2909826b65b43f33abb923a703a80894695c
andyluo882016 <andylu882016>
2022-02-14 12:46:34 -0800

    first commit v3 @2/16

--- src/main/java/com/app/MainRootTest.java
+++ src/main/java/com/app/MainRootTest.java
@@ -2,7 +2,7 @@ package com.app;
 
 public class MainRootTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args){//
         System.out.println("this is root top paranet");
         System.out.println("Add one new line for testing at root class");
     }
